59f1a78427c1c6ca6374b553bce1bc9b3732490e
thaddavis <thaddadavis@gmail.com>
2016-07-22 19:23:17 -0400

    add testing to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722190220) do
+ActiveRecord::Schema.define(version: 20160722225907) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 20160722190220) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(user_name: "Example User", email: "user@example.com", password: "password_field")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
